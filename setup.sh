bae5605496337ef6f1045fd533b17fecd4e5912a
Paulchen <lukas.23022005@gmail.com>
2025-02-25 15:53:10 +0100

    Add `.p10k.zsh` and `.zshrc` settings files

--- setup.sh
+++ setup.sh
@@ -17,3 +17,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# copy .zshrc file
+curl https://raw.githubusercontent.com/Paulchen5/shellSetup/refs/heads/main/.zshrc > ~/.zshrc
+# copy .p10k.zsh file
+curl https://raw.githubusercontent.com/Paulchen5/shellSetup/refs/heads/main/.p10k.zsh > ~/.p10k.zsh
