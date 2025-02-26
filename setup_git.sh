b92ccbf7d30012e7a3571db8a67f2489a3c0651f
Paulchen <lukas.23022005@gmail.com>
2025-02-26 09:29:59 +0100

    Create `setup_git.sh`

---
+++ setup_git.sh
@@ -0,0 +1,3 @@
+curl -fsSL "https://raw.githubusercontent.com/Paulchen5/config/main/.gitconfig" > ~/.gitconfig
+curl -fsSL "https://raw.githubusercontent.com/Paulchen5/config/main/.gitconfig.sap" > ~/.gitconfig.sap
+curl -fsSL "https://raw.githubusercontent.com/Paulchen5/config/main/.gitconfig.personal" > ~/.gitconfig.personal
